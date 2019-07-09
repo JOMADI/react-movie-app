97df47b9bad2f5d5f091f752f727cfde075b8635
JOMADI <victorjo603@gmail.com>
2019-07-09 14:43:10 +0100

    Created Movie Componets and added to Home

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+// eslint-disable-next-line
+import {FontAwesome} from '@fortawesome/react-fontawesome';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div>
+                SearchBar
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
